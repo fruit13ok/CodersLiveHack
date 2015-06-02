53e829b5c9404e56c9712c38650c7a18966f19ca
Yi Liu <fruit13ok@gmail.com>
2015-06-01 17:29:18 -0700

    first commit

---
+++ src/com/proto/coderslivehack/CodersLiveHackActivity.java
@@ -0,0 +1,15 @@
+package com.proto.coderslivehack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CodersLiveHackActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_coders_live_hack);
+	}
+}
